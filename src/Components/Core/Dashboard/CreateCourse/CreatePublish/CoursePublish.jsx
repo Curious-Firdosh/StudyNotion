019141e67b76c0e88cbbf09438571027baef5f0e
Curious-Firdosh <thefirdosh@gmail.com>
2025-05-16 22:36:51 +0530

    "Compleaed The Course "

--- src/Components/Core/Dashboard/CreateCourse/CreatePublish/CoursePublish.jsx
+++ src/Components/Core/Dashboard/CreateCourse/CreatePublish/CoursePublish.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CoursePublish = () => {
   return (
     <div>
-      <h1> Step 3 Mian Swagat Hai</h1>
+      <h1 className='text-4xl text-caribbeangreen-200 font-bold'> Step 3 Mian Swagat Hai</h1>
     </div>
   )
 }
